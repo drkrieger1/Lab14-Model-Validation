18d233a55b377e17961bafffebffc6a49d769860
drkrieger <eplyushko@gmail.com>
2017-09-29 11:42:07 -0700

    Validation added

--- Lab14Erik/Lab14Erik/Models/Student.cs
+++ Lab14Erik/Lab14Erik/Models/Student.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lab14Erik.Models
 {
     public class Student
-    {
+    {   
+
         public int ID { get; set; }
+
+        [StringLength(30, MinimumLength = 2)]
+        [Required]
         public string Name { get; set; }
+
+        [Range(17, 100)]
         public int Age { get; set; }
+
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
         public string Course { get; set; }
     }
 }
